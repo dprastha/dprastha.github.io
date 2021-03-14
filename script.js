a07e9641b6305856bb6daa45efbed3214246815e
dprastha <danielprastha4@gmail.com>
2021-03-14 18:45:53 +0700

    version 2.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// nama
+// * style hover name
 const nama = document.querySelector('.nama');
 const huruf = [...nama.textContent].map(h => `<span class="hov">${h}</span>`).join('');
 nama.innerHTML = huruf;
@@ -11,8 +11,38 @@ const nama3 = document.querySelector('.nama3');
 const huruf3 = [...nama3.textContent].map(h => `<span class="hov">${h}</span>`).join('');
 nama3.innerHTML = huruf3;
 
-const space = document.querySelectorAll('.nama2 .hov')[3];
+const space = document.querySelectorAll('.nama2 .hov')[4];
 space.classList.add('space');
 
 const space2 = document.querySelectorAll('.nama3 .hov')[3];
 space2.classList.add('space');
+
+// * Responsive Navbar
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+menuToggle.addEventListener('click', function () {
+   nav.classList.toggle('slide');
+});
+
+// * My work
+window.addEventListener('scroll', function () {
+   let scroll = window.pageYOffset;
+   console.log(scroll);
+
+   const header = document.querySelector('.header-mywork');
+   console.log(header.offsetTop);
+
+   if (scroll > header.offsetTop) {
+      header.classList.add('muncul');
+   }
+});
+
+// $(window).scroll(function () {
+//    let wScroll = $(this).scrollTop();
+//    console.log(wScroll)
+
+//    // header
+//    if (wScroll > $('.mywork').offset().top - 200) {
+//       $('.header-mywork').addClass('muncul');
+//    }
+// });
